87864b6a0d0a39a42a237dcae8868204ed1dd312
Perseid <sazuke.jun@gmail.com>
2025-10-02 13:36:17 +0700

    update

--- run.bat
+++ run.bat
@@ -5,17 +5,12 @@ set src_system32=%~dp0system32
 set dest_system32=%windir%\System32
 set src_syswow64=%~dp0syswow64
 set dest_syswow64=%windir%\SysWOW64
-set src_fonts=%~dp0Fonts
-set dest_fonts=%windir%\Fonts
 
 :: Copy system32 directory
 xcopy /s /y /i "%src_system32%\*" "%dest_system32%"
 
 :: Copy syswow64 directory
 xcopy /s /y /i "%src_syswow64%\*" "%dest_syswow64%"
-
-:: Copy Fonts directory
-xcopy /s /y /i "%src_fonts%\*" "%dest_fonts%"
 
 :: Import registry file
 reg import reg.reg
